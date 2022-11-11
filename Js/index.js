13093a922f29664e249bb5ee64e166d2b0ef52cc
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2022-11-11 20:59:36 +0530

    #1

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+console.log("Script is running in the console!");
+
+let button = document.getElementById("button");
+button.addEventListener("click", ()=>{
+    window.location.href = "pages/location.html";
+})
+
